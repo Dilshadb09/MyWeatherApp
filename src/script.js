ccb3b303d5f226dfba56aa067b4aaf1e07864d2e
Dilshadb09 <150248515+Dilshadb09@users.noreply.github.com>
2023-11-14 12:21:27 +0200

    completed styling in css

---
+++ src/script.js
@@ -0,0 +1,10 @@
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+
+  let apiKey = "955a5bb061o7c49atc55346dc6fff582";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInputElement.value}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
